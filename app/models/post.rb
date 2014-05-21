b34d74684032558199fe0a5d5e57b1b8d92adab3
Reza Jatnika <rezajatnika@gmail.com>
2014-05-21 12:52:48 +0700

    Run latest migration and annotate models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  excerpt    :string(255)
+#  user_id    :integer
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Post < ActiveRecord::Base
   # Associations
   belongs_to :user
--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: terms
+#
+#  id          :integer          not null, primary key
+#  post_id     :integer
+#  category_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Term < ActiveRecord::Base
   # Associations
   belongs_to :post
