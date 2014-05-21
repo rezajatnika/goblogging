8604331ed946ba29acbbcd5be0ccd2b0137aca9c
Reza Jatnika <rezajatnika@gmail.com>
2014-05-21 13:37:29 +0700

    Add require_logout method to sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  # Preventing current_user to log in again, a before filter
+  def require_logout
+    redirect_to :back, notice: 'Already logged in!' if current_user
+  end
+
+  # Callback method for not authenticated user
+  def not_authenticated
+    redirect_to login_path, alert: 'Please log in first.'
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_filter :require_logout, only: [:new, :create]
+
   # GET /sessions/new
   def new
     @user = User.new
